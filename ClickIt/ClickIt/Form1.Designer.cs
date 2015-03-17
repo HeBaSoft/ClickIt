8bf9d2655b48f558d3e3c5ca361eeab7d259c31e
MichiHory <Mich.horak@email.cz>
2015-03-17 18:56:59 +0100

    Initial upload

---
+++ ClickIt/ClickIt/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ClickIt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.keysBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.keysBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mouseProtect = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Poslední klávesa";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // keysBox1
+            // 
+            this.keysBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.keysBox1.FormattingEnabled = true;
+            this.keysBox1.Location = new System.Drawing.Point(74, 38);
+            this.keysBox1.Name = "keysBox1";
+            this.keysBox1.Size = new System.Drawing.Size(99, 21);
+            this.keysBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Čas (ms)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Klávesa 1";
+            // 
+            // keysBox2
+            // 
+            this.keysBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.keysBox2.FormattingEnabled = true;
+            this.keysBox2.Location = new System.Drawing.Point(74, 65);
+            this.keysBox2.Name = "keysBox2";
+            this.keysBox2.Size = new System.Drawing.Size(99, 21);
+            this.keysBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Klávesa 2";
+            // 
+            // mouseProtect
+            // 
+            this.mouseProtect.AutoSize = true;
+            this.mouseProtect.Checked = true;
+            this.mouseProtect.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mouseProtect.Location = new System.Drawing.Point(21, 101);
+            this.mouseProtect.Name = "mouseProtect";
+            this.mouseProtect.Size = new System.Drawing.Size(123, 17);
+            this.mouseProtect.TabIndex = 10;
+            this.mouseProtect.Text = "Mouse move protect";
+            this.mouseProtect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 166);
+            this.Controls.Add(this.mouseProtect);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.keysBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keysBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Click It";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox keysBox2;
+        private System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.ComboBox keysBox1;
+        private System.Windows.Forms.CheckBox mouseProtect;
+    }
+}
+
---
+++ ClickIt/ClickIt/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ClickIt
+{
+    public partial class Form1 : Form      
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int vKey);
+        [DllImport("user32.dll")]
+        static extern byte VkKeyScan(char ch);
+        public const int Press = 0x0001; //Key down flag
+        public const int Release = 0x0002; //Key up flag   
+        string keyBuffer = "";
+        private float mouseX;
+        private float mouseY;
+
+        public Form1()       
+        {
+            
+            InitializeComponent();
+            var seznam = new Dictionary<string, byte>()
+            {
+                {"None", 0},
+                {"MouseLeft", 0x02},
+                {"MouseRight", 0x08},
+                {"LCtrl", 0xA2},
+                {"X", 0x58}
+            };
+            keysBox1.DataSource = seznam.ToList();
+            keysBox1.DisplayMember = "Key";
+            keysBox1.ValueMember = "Value";
+
+            keysBox2.DataSource = seznam.ToList();
+            keysBox2.DisplayMember = "Key";
+            keysBox2.ValueMember = "Value";
+            keysBox1.SelectedIndex = 1;
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt16(numericUpDown1.Value);
+            timer1.Interval = i;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            byte hodnota = Convert.ToByte(keysBox1.SelectedValue);
+            byte hodnota2 = Convert.ToByte(keysBox2.SelectedValue);
+            if (mouseProtect.Checked == true)
+            {
+                if(mouseX + 40 < MousePosition.X || mouseX - 40 > MousePosition.X || mouseY + 40 < MousePosition.Y || mouseY - 40 > MousePosition.Y)
+                {
+                    hodnota = 0;
+                    hodnota2 = 0;
+                }              
+            }
+
+            if (hodnota == 0 && hodnota2 == 0)
+            {
+                timer1.Stop();
+                keyBuffer = "NumPad2";
+            }          
+            else{ 
+                
+                keybd_event(hodnota, 0, 0, 0);
+                keybd_event(hodnota2, 0, 0, 0);
+                keybd_event(hodnota2, 0, Release, 0);
+                keybd_event(hodnota, 0, Release, 0);                             
+                
+                if (hodnota == 0x02 || hodnota2 == 0x02)
+                {
+                    mouse_event(hodnota, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                    mouse_event(0x04, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                }
+                if (hodnota == 0x08 || hodnota2 == 0x08)
+                {
+                    mouse_event(hodnota, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                    mouse_event(0x10, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+                }
+            }
+        }           
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+                       
+            foreach (System.Int32 i in Enum.GetValues(typeof(Keys)))
+            {
+                if (GetAsyncKeyState(i) == -32767)
+                keyBuffer = Enum.GetName(typeof(Keys), i);
+            }
+            label1.Text = keyBuffer;
+            if (keyBuffer == "NumPad1")
+            {
+                mouseX = MousePosition.X;
+                mouseY = MousePosition.Y;
+                int i = Convert.ToInt16(numericUpDown1.Value);
+                timer1.Interval = i;
+                timer1.Start();
+                keyBuffer = "RUN";
+            }
+            else if (keyBuffer == "NumPad2")
+            {
+                timer1.Stop();
+            }
+            else { }
+        }
+    }
+}
